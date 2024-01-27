26af4c1c1ff8545fba8420203387c1da8f7fd6c1
182Marco <marcomilza@gmail.com>
2024-01-27 21:11:12 +0100

    first hello world done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TryComponent } from './try/try.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TryComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-course';
---
+++ src/app/try/try.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-try',
+  standalone: true,
+  imports: [],
+  templateUrl: './try.component.html',
+  styleUrl: './try.component.scss'
+})
+export class TryComponent {
+
+}
